3d3181c8e32925071caabfd604dc29b3d03406eb
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-04-20 02:02:51 -0600

    adding toast for createcitizen component

--- src/app/pages/citizen/createcitizen/createcitizen.component.ts
+++ src/app/pages/citizen/createcitizen/createcitizen.component.ts
@@ -4,6 +4,7 @@ import { Citizen } from 'src/app/models/citizen';
 import { City } from 'src/app/models/city';
 import { CitiesService } from 'src/app/services/cities/cities.service';
 import { CitizenService } from 'src/app/services/citizen/citizen.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-createcitizen',
@@ -24,6 +25,7 @@ export class CreatecitizenComponent implements OnInit {
     private citizenService: CitizenService,
     private router: Router,
     private cityservice: CitiesService,
+    private toastr: ToastrService,
     ) {
       this.citizen = new Citizen();
 
@@ -53,7 +55,12 @@ selectedChangeCity(){
    debugger;
     this.citizenService.createCitizen(this.citizen)
       .subscribe((res: any) => {
-     
+        if(res=="Added sucefully"){
+          this.toastr.success(res, 'Citizen');
+        }else{
+          this.toastr.error(res, 'Citizen');
+
+        }
       });
       this.router.navigate(['citizen']).then(() => {
         window.location.reload();
