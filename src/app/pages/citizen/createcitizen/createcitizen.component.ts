c172224500b68d98eb11487418fb891c72a01fd2
DESKTOP-AICR3S2\alexe <javc1603@gmail.com>
2021-04-20 23:21:14 -0600

    restriction for field DNI

--- src/app/pages/citizen/createcitizen/createcitizen.component.ts
+++ src/app/pages/citizen/createcitizen/createcitizen.component.ts
@@ -68,4 +68,13 @@ selectedChangeCity(){
   
  
   }
+
+  numberOnly(event): boolean {
+    const charCode = (event.which) ? event.which : event.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+      return false;
+    }
+    return true;
+
+  }
 }
